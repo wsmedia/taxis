3e13539d4b0190dcb1a1109d16a2bfb2ea6f8bbf
Bryan Mahoney <bryan@goDynamo.com>
2012-02-28 20:05:53 -0500

    Remove jquery:install from installation script. Adds dependency for jquery-rails

--- lib/generators/taxis/install_generator.rb
+++ lib/generators/taxis/install_generator.rb
@@ -15,28 +15,6 @@ module Taxis
       
     end
 
-    def ensure_jquery_gem
-      unless options[:skip_jquery]
-        if ! defined? Jquery
-          gem 'jquery-rails', '>= 0.2.6'
-
-          jqry_msg = <<-TXT
-The taxis default admin views require jquery for their use. As such, jquery-rails
-was added to your gemfile.  You need to run "bundle install" and rerun this generator
-to finish installing taxis. If you don't want to use jquery, you can provide the --skip-jquery
-option when running the generator, but this will break the admin views.
-NOTE: This will also remove the prototype javascript framework from your application.
-See https://github.com/indirect/jquery-rails for more information.
-TXT
-
-
-          say jqry_msg, :red
-          exit
-        else
-          generate "jquery:install"
-        end
-      end
-    end
     
     def copy_taxonomy_js
       copy_file 'taxonomy.js', 'app/assets/javascripts/taxonomy.js'
