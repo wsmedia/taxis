a8414624b1020c91c70b04261f367574640b0dd8
Bryan Mahoney <bryan@goDynamo.com>
2012-03-06 20:59:52 -0500

    Removes taxonomies controller which is now handled by active_admin

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,9 @@ Rails.application.routes.draw do
     resources :taxons do
       get :children, :on => :member
     end
-
-    resources :taxonomies do
-      resources :taxons
-    end
   end
 
 
-  root :to => "admin/taxonomies#index"
+  root :to => "admin/taxons#index"
   
 end
